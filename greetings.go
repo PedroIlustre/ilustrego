2583b85ca829470dfdf17c0067b7c8c1c0926037
Giuli Ilustre <ilustre.giuli@gmail.com>
2020-10-02 20:25:43 -0300

    Cálculo de IMC - versão 0.1

--- greetings.go
+++ greetings.go
@@ -5,16 +5,31 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("Vamu la!")
-<<<<<<< HEAD
-	fmt.Println("Função IF")
-	x := 10
-	if x > 1 {
-		fmt.Println("E não precisa colocar parenteses na condição")
+	var alt =0.00
+	var peso =0.00
+	var result=0.00
+	fmt.Println("*********************************************************")
+	fmt.Println("----------------Calculadora de IMC-----------------------")
+	fmt.Println("*********************************************************")
+
+	fmt.Println("Digite sua altura em centímetros (exemplo: 1,75m = 175cm)")
+	fmt.Scanln(&alt)
+	fmt.Println("Digite seu peso em gramas (exemplo: 95,80kg = 9580g)")
+	fmt.Scanln(&peso)
+	result = (alt * alt) / peso
+
+	if result > 18.50 && result < 24.90 {
+		fmt.Printf(" Seu IMC é %.2f. Você tem o peso normal.",result)
+	} else if result > 25 && result < 29.99 {
+		fmt.Printf(" Seu IMC é %.2f. Você está com sobrepeso.",result)
+	} else if result > 30 && result < 34.90 {
+		fmt.Printf(" Seu IMC é %.2f. Você tem Obesidade Grau I .",result)
+	} else if result > 35 && result < 39.99 {
+		fmt.Printf(" Seu IMC é %.2f. Você tem Obesidade Grau II .",result)
+	} else if result > 40 {
+		fmt.Printf(" Seu IMC é %.2f. Você tem Obesidade Grau III, CUIDADO!!!",result)
 	}
-||||||| merged common ancestors
-=======
-	fmt.Println("taan tana nana nana nana")
->>>>>>> e8bb81cf6982effe9817497f2fd76996ba8c485e
+
 }
+
+// No "GO playground" deu certo, mas não deu pra ler os valores, testa no teu ambiente vê se funciona ;)
